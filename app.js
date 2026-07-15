7fbc19ac3d4754e043a65eae670d7acf4eed6084
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-07-15 19:40:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/users", userRoutes);
+
+module.exports = app;
